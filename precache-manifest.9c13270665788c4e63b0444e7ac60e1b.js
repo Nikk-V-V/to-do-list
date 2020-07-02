2d3423db2f38f0f36447589ced1a1d62f92b4f8f
NikolayBlackJack <44206804+NikolayBlackJack@users.noreply.github.com>
2020-07-02 03:03:48 +0300

    deploy23

---
+++ precache-manifest.9c13270665788c4e63b0444e7ac60e1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57058bbb51c191fb05e4",
+    "url": "/vue_to-do-list/css/app.0b193f5b.css"
+  },
+  {
+    "revision": "0e891878dc33dcfaebbc",
+    "url": "/vue_to-do-list/css/chunk-78b2294a.3b258d3b.css"
+  },
+  {
+    "revision": "69b9490ff022bc2fafe5816a0f12845c",
+    "url": "/vue_to-do-list/index.html"
+  },
+  {
+    "revision": "57058bbb51c191fb05e4",
+    "url": "/vue_to-do-list/js/app.d4465287.js"
+  },
+  {
+    "revision": "0e891878dc33dcfaebbc",
+    "url": "/vue_to-do-list/js/chunk-78b2294a.f5d4c85f.js"
+  },
+  {
+    "revision": "1fe3fd2964791538149c",
+    "url": "/vue_to-do-list/js/chunk-vendors.92aa18ee.js"
+  },
+  {
+    "revision": "27c37c5d443ad2925e3d8b771894b808",
+    "url": "/vue_to-do-list/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_to-do-list/robots.txt"
+  }
+]);
